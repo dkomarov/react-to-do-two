8f4712307262aca16a86ab0ee2772f43c8bf6c65
dkomarov <dkomarov@hawk.iit.edu>
2020-07-27 14:45:14 -0500

    add main component file
    includes three functions: getInitialState, onDelete, & render

---
+++ src/app/index.js
@@ -0,0 +1,43 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+require('./css/index.css')
+
+
+// module require statements
+// import TodoItem from './todoItem'
+
+// Create component
+const TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers']
+    }
+  }, 
+  onDelete: function(item){
+    var updatedTodos = this.state.todos.filter(function(val, index){
+      return item !== val;
+    });
+    this.setState({
+      todos: updatedTodos
+    });
+  },
+  // render method
+  render: function(){
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+      );
+    }.bind(this));
+    return(
+      <div id='todo-list'>
+        <p>The busiest people have the most leisure...</p>
+        <p>{this.state.age}</p>
+        <ul>{todos}</ul>
+      </div>
+    );
+  } // render()
+});
+
+
+// insert component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))
